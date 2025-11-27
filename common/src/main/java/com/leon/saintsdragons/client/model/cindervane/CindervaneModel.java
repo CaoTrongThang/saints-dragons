5e7330dffe5181b15e68612e4d846d9213a6acc7
Saint <zackryan512@gmaill.com>
2025-11-27 16:58:28 +0700

    BRU

--- common/src/main/java/com/leon/saintsdragons/client/model/cindervane/CindervaneModel.java
+++ common/src/main/java/com/leon/saintsdragons/client/model/cindervane/CindervaneModel.java
@@ -94,7 +94,7 @@ public class CindervaneModel extends DefaultedEntityGeoModel<Cindervane> {
 
         applyNeckBoneRotation("neck1", neckLeanRad * 0.5f);
         applyNeckBoneRotation("neck2", neckLeanRad * 1.0f);
-        applyNeckBoneRotation("skullController", neckLeanRad * 1.25f);
+        applyNeckBoneRotation("skull", neckLeanRad * 1.25f);
     }
 
     private void applyGroundNeckTurn(Cindervane entity, float partialTick) {
@@ -132,7 +132,7 @@ public class CindervaneModel extends DefaultedEntityGeoModel<Cindervane> {
         applyNeckBoneFollow("neck1", lookPitchRad, totalYawRad, 0.35f);
         applyNeckBoneFollow("neck2", lookPitchRad, totalYawRad, 0.55f);
         applyNeckBoneFollow("neck3", lookPitchRad, totalYawRad, 0.70f);
-        applyNeckBoneFollow("skullController", lookPitchRad, totalYawRad, 0.80f);
+        applyNeckBoneFollow("skull", lookPitchRad, totalYawRad, 0.80f);
     }
 
     private void applyNeckBoneFollow(String boneName, float headDeltaX, float headDeltaY, float weight) {
