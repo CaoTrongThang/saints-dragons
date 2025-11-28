9d1fe8a71d1db7ce6ce8ba1856f318050ff694e5
Saint <zackryan512@gmaill.com>
2025-11-28 10:14:32 +0700

    First config fix

--- neoforge/src/main/java/com/leon/saintsdragons/neoforge/SaintsDragonsNeoForge.java
+++ neoforge/src/main/java/com/leon/saintsdragons/neoforge/SaintsDragonsNeoForge.java
@@ -7,6 +7,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -34,6 +36,9 @@ public class SaintsDragonsNeoForge {
         modContainer.registerConfig(ModConfig.Type.COMMON,
                 com.leon.saintsdragons.neoforge.platform.NeoForgeConfigHelper.SPAWN_SPEC,
                 "saintsdragonsspawning.toml");
+
+        // Register config screen for in-game editing
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 
         // Dragon attributes are configured via config/saintsdragons/dragon_attributes/*.json
         // No TOML config needed - JSONs are the source of truth
