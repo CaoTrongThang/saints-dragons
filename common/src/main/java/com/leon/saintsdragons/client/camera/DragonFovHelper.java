2491dfdaa2fb5cd375c8d7d71659564812633bb8
Saint <zackryan512@gmaill.com>
2026-02-02 21:51:17 +0700

    Egg fixing + FOV unification.

--- common/src/main/java/com/leon/saintsdragons/client/camera/DragonFovHelper.java
+++ common/src/main/java/com/leon/saintsdragons/client/camera/DragonFovHelper.java
@@ -5,7 +5,7 @@ import com.leon.saintsdragons.server.entity.interfaces.RideableDragon;
 import net.minecraft.world.entity.Entity;
 
 public final class DragonFovHelper {
-    public static final double GROUND_SPRINT_MULTIPLIER = 1.05;
+    public static final double GROUND_SPRINT_MULTIPLIER = 1.050;
     public static final double FLY_SWIM_SPRINT_MULTIPLIER = 1.075;
 
     private DragonFovHelper() {}
